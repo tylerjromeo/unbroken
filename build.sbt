168a1ee85d3a186d40fc8f87987e497cdba7b163
Tyler Romeo <tyler.romeo@corvesta.com>
2019-08-05 12:30:43 -0400

    model game state WIP

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "unbroken"
 version := "0.1"
 
 scalaVersion := "2.13.0"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.28"
--- src/main/scala/org.romeo.unbroken/state/GameState.scala
+++ src/main/scala/org.romeo.unbroken/state/GameState.scala
@@ -1,6 +1,9 @@
 package org.romeo.unbroken.state
 
 import org.romeo.unbroken.state.Level.Level
+import org.romeo.unbroken.state.WeaponType.WeaponType
+
+import scalaz.State
 
 case class GameState(resources: Resources,
                      effort: Effort,
@@ -20,9 +23,24 @@ case class Effort(small: Int, medium: Int, large: Int)
 
 case class DeckState()
 
-case class Weapon()
+case class Weapon(name: String, weaponType: WeaponType, attacks: Seq[WeaponAttack])
 
-case class PlayerCharacter()
+object WeaponType extends Enumeration {
+  type WeaponType = Value
+  val BareHands, Basic, Advanced = Value
+}
+
+abstract class WeaponAttack(cost: Cost) {
+  def weaponEffect: State[GameState, Monster]
+}
+
+case class PlayerCharacter(name: String, maxAbilityUses: Int, abilityUsesRemaining: Int, abilities: Seq[CharacterAbility])
+
+abstract class CharacterAbility(cost: Cost) {
+  type T
+  def getReturnType: Class[T] = Class[T]
+  def abilityEffect: State[GameState, T]
+}
 
 case class Skill()
 
@@ -31,6 +49,8 @@ case class Condition()
 case class Monster()
 
 case class Phase()
+
+case class Cost()
 
 object Level extends Enumeration {
   type Level = Value
